6d16187be658583a8b6b96d40285c66ab1678dce
Nonthakit Sivaratanatamrong <NonthakitS@arv.co.th>
2023-06-26 16:54:25 +0700

    build

---
+++ packages/ts-migrate-extra/build/scripts/jest-config-reignore.d.ts
@@ -0,0 +1,6 @@
+export let rootDir: string;
+export let runner: string;
+export let displayName: string;
+export let testEnvironment: string;
+export let moduleFileExtensions: string[];
+export let testMatch: string[];
